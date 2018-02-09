6751281aa26b3289663a62af7f52033e84c530ec
Caio Guerra <caiosouza1106@hotmail.com>
2018-02-09 17:43:11 -0200

    Update GREP-IF.sh

--- GREP-IF.sh
+++ GREP-IF.sh
@@ -11,42 +11,42 @@ echo $NAME | grep  -E '^([[:alpha:]]|[[:space:]])'
 read -p "Email: " MAIL
 echo  MAIL | grep -E '^.{1,}@(hotmail|outlook|gmail)/.(com|br|com.br)'
     if [[$? = 0 ]]; then 
-        echo "Login Correto"
+        echo "Email Correto"
         else
-        echo "Login Incorreto"
+        echo "Email Incorreto"
     fi
  read -p "Telefone: " FONE
  echo FONE | grep -E '^\([0-9]{2}\)[0-9]{5}-[0-9]{4}'
        if [[$? = 0 ]]; then 
-        echo "Login Correto"
+        echo "Telefone Correto"
         else
-        echo "Login Incorreto"
+        echo "Telefone Incorreto"
     fi
 read -p "RG: " RG
 echo RG | gre -E '^[0-9]{2}(\.[0-9]{3}){2}-[0-9]' 
    if [[$? = 0 ]]; then 
-        echo "Login Correto"
+        echo "RG Correto"
         else
-        echo "Login Incorreto"
+        echo "RG Incorreto"
     fi
     read -p "CPF: " CPF
 echo CPF | gre -E '^[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}' 
    if [[$? = 0 ]]; then 
-        echo "Login Correto"
+        echo "CPF Correto"
         else
-        echo "Login Incorreto"
+        echo "CPF Incorreto"
     fi
     read -p "IP: " IP
 echo IP | gre -E '^[0-9]{2,3}\.[0-9]{2,3}\.[0-9]{2,3}\.[0-9]{2,3}' 
    if [[$? = 0 ]]; then 
-        echo "Login Correto"
+        echo "IP Correto"
         else
-        echo "Login Incorreto"
+        echo "IP Incorreto"
     fi
     read -p "Mascara:  " MASK
 echo MASK | gre -E '^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\.[0-9]{3}' 
    if [[$? = 0 ]]; then 
-        echo "Login Correto"
+        echo "Mascara Correto"
         else
-        echo "Login Incorreto"
+        echo "Mascara Incorreto"
     fi
