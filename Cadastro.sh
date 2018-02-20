f05008839e24bb329cbdc2f997a3eb85ecaba135
CAIO HENRIQUE GUERRA DE SOUZA <47613851893@jpmorgan.sp>
2018-02-20 17:26:55 -0300

    Terminado

--- Cadastro.sh
+++ Cadastro.sh
@@ -3,17 +3,18 @@
 echo        "Cadastro de pessoas"
 sleep 2
 clear
+
 read -p "Digite seu nome: " NAME
 echo $NAME | grep  -E '^([a-z|A-Z]{1,}\ [a-z|A-Z]{1,}|[a-z|A-Z]{1,}){1,}$';
     if [[  $? -eq 0  ]]; then
-        echo "Nome Correto"
+        echo "Login Correto"
         else
-        echo "Nome Incorreto"
+        echo "Login Incorreto"
         
     fi
 
 read -p "Data de Nascimento: " DATA
-echo $DATA | grep -E '^[0-9]{2}/[0-9]{2}/[0-9]{2,4}$'
+echo $DATA | grep -E '^(0[0-9]|[1,2][0-9]|3[0-1])/(0[0-9]|1[0-2])/(19[0-9]{2}|200[0-9]|201[0-8])$'
     if [[ $? -eq 0 ]]; then
         echo "Data de Nascimento Correto"
         else 
@@ -55,9 +56,8 @@ echo $PFC | grep  -E '^([0-9]{3}\.){2}[0-9]{3}-[0-9]{2}$';
         echo "CPF Incorreto"
         
     fi
-    
 read -p "IP: " PI
-echo $PI | grep  -E '^([0-254]{1,3}\.){3}[0-254]{1,3}$';
+echo $PI | grep  -E '^([0-9]\.|[1-9][0-9]\.|1[0-9]{2}\.|2[0-4][0-9]\.|25[0-5]\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2{0-4}[0-9]|25[0-4])$';
     if [[  $? -eq 0  ]]; then
         echo "IP Correto"
         else
@@ -66,7 +66,7 @@ echo $PI | grep  -E '^([0-254]{1,3}\.){3}[0-254]{1,3}$';
     fi
     
 read -p "MASCARA: " MASC
-echo $MASC | grep  -E '^([0-255]{1,3}\.){3}[0-255]{1,3}$';
+echo $MASC | grep  -E '^(25[0-5]{1,3}\.){2}([0-255]{1,3}\.)[0-255]{1,3}$';
     if [[  $? -eq 0  ]]; then
         echo "Mascara Correto"
         else
